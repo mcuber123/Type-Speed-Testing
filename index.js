b0c291c46b6a023be6a2f012992450a47a2d1f55
mcuber123 <74697645+mcuber123@users.noreply.github.com>
2020-11-30 15:48:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+
+console.log("Hello Nodejs this side");
+
+
+const arr1=[2,3,4,5]
+//console.log(Array.isArray(arr));
+arr2=[].concat(arr1);
+console.log(arr2);
+const arr3=arr2.concat(arr1);
+console.log(arr3);
